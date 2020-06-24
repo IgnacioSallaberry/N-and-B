d4c85c96e938d1fee470897f758fdf4b24a80229
IgnacioSallaberry <42651786+IgnacioSallaberry@users.noreply.github.com>
2020-06-24 20:36:07 -0300

    Funciones de N&B
    N&B = recibe un array de matrices y calcula N&B. Por defecto la calibración es para el modo photon counting (S=1, sigma_0=0 y offset=0), caso contrario hay que ingresar los valores.
    
    N&B_MAV= Falta implementar. La idea es que haga un MAV.
    
    is_grey_scale = función para saber si la imagen que cargamos está en RGB o en escala de grises

--- funcion_de_NandB.py
+++ funcion_de_NandB.py
@@ -2,80 +2,99 @@
 """
 Created on Sat Apr 18 14:21:58 2020
 
-@author: ETCasa
+@author: Ignacio Sallaberry
 """
 import numpy as np
-from PIL import Image
 
-
-def NandB (name):
+def NandB (imarray,calibration='Photon Count'):
 
 #==============================================================================
 #                                CALIBRACION DE DETECTOR
 #==============================================================================    
-    detector_offset = 0
-    S_factor = 1
-    sigma_cero = 0
+    if calibration == 'Photon Count':
+        detector_offset = 0
+        S_factor = 1
+        sigma_cero = 0
     
-    #detector_offset = 0.197
-    #S_factor = 4.008
-    #sigma_cero = 0.002
+#    elif calibration == 'UNSAM':
+#        detector_offset = 0.197
+#        S_factor = 4.008
+#        sigma_cero = 0.002
     
-    
-####  ------------------------    PARA UNA IMAGEN    -------------------
-    im = Image.open(name)   #importa la imagen .tif
-    im = np.array(im) #crea una matriz de valores de la imagen importada
-    imarray = [im.tolist()]
-####  ------------------------    armo los nombres de los archivos    -------------------
-    i=2
-    indice=[]
-    while i<101:
-        if i<10:
-            indice.append(f'00{i}.tif')
-        elif 9<i<100:
-            indice.append(f'0{i}.tif')
-        else:
-            indice.append(f'{i}.tif')
-        i+=1
-####  ------------------------    armo array con las matrices de las 100 imágenes  -------------------
-    i=1
-    while i<len(indice):
-        im = Image.open(name[:len(name)-7]+indice[i])
-        im = np.array(im) #crea una matriz de valores de la imagen importada
-        imarray.append(im.tolist())
-        print(i)    
-        i+=1
-    imarray = np.array(imarray)
+    else:
+        detector_offset = np.float(input ("Enter Offset value :"))
+        S_factor = np.float(input ("Enter S value :"))
+        sigma_cero = np.float(input ("Enter Sigma_0  value :"))
+        
     
     STD = np.std(imarray, axis=0)   #axis=0   recorre en profundidad la matriz en 3D----> numpy.sum(matriz, axis=0)
     k_medio = np.mean(imarray, axis=0)
-    B = np.divide(STD**2-sigma_cero**2,k_medio-detector_offset)
+    VAR=np.var(imarray, axis=0)
+
+
+#    B = np.divide((STD**2+sigma_cero**2)*S_factor**2,(k_medio+detector_offset)*S_factor)
+    B = np.divide((VAR+sigma_cero**2)*S_factor,(k_medio+detector_offset))
+#    B = np.divide(STD**2-sigma_cero**2,k_medio-detector_offset)
+#    B = np.divide(VAR-sigma_cero**2,k_medio-detector_offset)     ## var = std**2
+    
+ 
     B = B.ravel()   #esto hace que el array de matrices se haga simplemente una array de valores del brillo
+    k_medio = k_medio.ravel()
     
-    return(STD, k_medio, B, imarray)
+    i=0
+    while i<len(B):
+        if np.isnan(B[i]):
+            B[i]=float(np.nan_to_num(B[i]))
+            i+=1
+        else:
+            i+=1
+            
+    return(STD, k_medio, B, VAR)
 
 
 
 
-def NandB_detrend (name):
+#def NandB_MAV (imarray,calibration='LELOIR'):
+#
+##==============================================================================
+##                                CALIBRACION DE DETECTOR
+##==============================================================================    
+#    if calibration == 'Photon Count':
+#        detector_offset = 0
+#        S_factor = 1
+#        sigma_cero = 0
+#    
+#    elif calibration == 'UNSAM':
+#        detector_offset = 0.197
+#        S_factor = 4.008
+#        sigma_cero = 0.002
+#    
+#    else:
+#        detector_offset = np.float(input ("Enter Offset value :"))
+#        S_factor = np.float(input ("Enter S value :"))
+#        sigma_cero = np.float(input ("Enter Sigma_0  value :"))
+#    
+#    
+#    STD = np.std(imarray, axis=0)   #axis=0   recorre en profundidad la matriz en 3D----> numpy.sum(matriz, axis=0)
+#    k_medio = np.mean(imarray, axis=0)
+#    VAR=np.var(imarray, axis=0)
+#
+##    B = np.divide((STD**2+sigma_cero**2)*S_factor**2,(k_medio+detector_offset)*S_factor)
+#    B = np.divide((VAR+sigma_cero**2)*S_factor**2,(k_medio+detector_offset)*S_factor)
+##    B = np.divide(STD**2-sigma_cero**2,k_medio-detector_offset)
+##    B = np.divide(VAR-sigma_cero**2,k_medio-detector_offset)     ## var = std**2
+#    
+#    B = B.ravel()   #esto hace que el array de matrices se haga simplemente una array de valores del brillo
+#    
+#    return(STD, k_medio, B, VAR)
 
-    
-#==============================================================================
-#                                CALIBRACION DE DETECTOR
-#==============================================================================    
-    detector_offset = 0
-    S_factor = 1
-    sigma_cero = 0
-    
-    #detector_offset = 0.197
-    #S_factor = 4.008
-    #sigma_cero = 0.002
-    
-    
-    STD = np.std(imarray, axis=0)
-    k_medio = np.mean(imarray, axis=0)
-    B = np.divide(STD**2-sigma_cero**2,k_medio-detector_offset)
-    B = B.ravel()   #esto hace que el array de matrices se haga simplemente una array de valores del brillo
-   
-    
-    return(STD, k_medio, B)
+### función para saber si la imagen que cargamos está en RGB o en escala de grises
+def is_grey_scale(img_path):
+    img = Image.open(img_path).convert('RGB')
+    w,h = img.size
+    for i in range(w):
+        for j in range(h):
+            r,g,b = img.getpixel((i,j))
+            if r != g != b: return False
+    return True
+
